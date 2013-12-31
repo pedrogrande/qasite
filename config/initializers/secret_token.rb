241cfe2ebecc72284b7ee5d945f6c2ea72c061c6
pedroargento <peter.argent@gmail.com>
2013-12-31 19:01:54 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qasite::Application.config.secret_key_base = '5d090e2f1d7c9512cdee396025252f2b07b6487811c6f87ee78521dbbb32449d90e532912266ed443d1089363004a0cea9274532319cfee842865b5b290cf7bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Qasite::Application.config.session_store :cookie_store, key: '_qasite_session'
